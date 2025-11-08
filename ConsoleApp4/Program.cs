1c3e8bc3bcff426ebd550d1e55c01da819c3fc5f
arthurcamposxz <arturcamposcampos121@gmail.com>
2025-11-08 17:16:00 -0300

    Exercicio 1 da primeira aula

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,49 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digire seu nome: ");
+            /*string nome = "Arthur";
+            const string saudacao = "Bem-Vindo";
+
+            Console.WriteLine("" + saudacao + ", " + nome + "!");
+
+            int numero1 = 10;
+            int numero2 = 10;
+            int resultado = numero1 + numero2;
+            Console.WriteLine("O resultado é igual: " + resultado);
+            
+
+            Console.WriteLine("Digite sua idade, por favor: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            int resultado = idade + 10;
+            Console.WriteLine("Daqui a 10 anos você terá: " + resultado + " anos.");
+            
+
+            double preco = 2000.00;
+            double desconto = 10.0;
+            double valorDescnto = (preco * desconto) / 100;
+            double precoFinal = preco - valorDescnto;   
+            Console.WriteLine("O preço final com desconto é: R$ " + precoFinal.ToString("F2", CultureInfo.InvariantCulture));
+            
+
+            const double pi = 3.14159;
+            Console.WriteLine("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double area = pi * raio * raio;
+            Console.WriteLine("A área do círculo é: " + area.ToString("F4", CultureInfo.InvariantCulture));
+            
+
+            int numero3 = 5;
+            string nome = "Arthurzin";
+            double valor = 10.35784;
+            bool condicao = true;
+            Console.WriteLine(numero3.GetType());
+            Console.WriteLine(nome.GetType());
+            Console.WriteLine(valor.GetType());
+            Console.WriteLine(condicao.GetType());
+            */
+
+            
         }
     }
 }
